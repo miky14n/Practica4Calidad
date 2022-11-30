e847f34d6c1ca274f5c795a4b0d9d1f2f3b1ea2e
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2022-11-30 17:09:07 -0400

    deleting unnecesary code

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,19 +4,6 @@ require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara-screenshot/cucumber'
 
-require "rubygems"
-require "watir"
-
-Before do |scenario|
-  browser = Watir::Browser.new :chrome
-  $browser = browser
-  $browser.driver.manage.window.maximize
-end
-
-After do |scenario|
-  $browser.close
-end
-
 #PTravel Settings
 ENV['USER']="lider@gmail.com"
 ENV['PSW']="123456"
